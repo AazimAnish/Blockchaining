8fbe3e6b82c6214c1b364a4f311cb432f68809d7
AazimAnish <aazimanish1@gmail.com>
2024-04-24 01:00:34 +0530

    contract interaction done

---
+++ StorageFactory.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import "./SimpleStorage.sol";
+
+
+contract StorageFactory {
+
+    SimpleStorage[] public listOfSimpleStorageContracts;
+
+    function createSimpleStorageContract() public {
+        SimpleStorage newSimpleStorageContract = new SimpleStorage();
+        listOfSimpleStorageContracts.push(newSimpleStorageContract);
+    }
+
+    function getListOfSimpleStorageContracts() public view returns (SimpleStorage[] memory) {
+        return (listOfSimpleStorageContracts);
+    }
+
+    function sfStore(uint256 _simpleStorageIndex, uint256 _newSimpleStorageNumber) public {
+        //address
+        //ABI
+       listOfSimpleStorageContracts[_simpleStorageIndex].store(_newSimpleStorageNumber);
+    }
+
+    function sfGet(uint256 _simpleStorageIndex) public view returns(uint256) {
+        return listOfSimpleStorageContracts[_simpleStorageIndex].retrieve();
+    }
+
+}
