9a8f68db0c44627eeeadca414412d9fb63d1c236
AazimAnish <aazimanish1@gmail.com>
2024-04-25 16:58:43 +0530

    libraries added

--- FundME.sol
+++ FundME.sol
@@ -6,13 +6,20 @@
 
 pragma solidity ^0.8.18;
 
+import {PriceConverter} from "./PriceConverter.sol";
+
 contract FundMe {
+    using PriceConverter for uint256;
+
+    uint256 public minimumUsd = 5e18;
+
+    address[] public funder;
+    mapping(address funder => uint256 amountFunded) public addressToAmountFunded;
 
     function fund() public payable{
-        // Allow users to send $
-        // Have a minimum $ to sent
-
-        require(msg.value > 1e18, "Didn't send enough ETH");
+        require(msg.value.getConversionRate() >=  minimumUsd, "Didn't send enough ETH");
+        funder.push(msg.sender);
+        addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value;
     }
 
     //function withdraw() public {}
